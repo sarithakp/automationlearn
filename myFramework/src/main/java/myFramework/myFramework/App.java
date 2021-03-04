13af4651164469e07bfd60781bd82f43c986776a
DESKTOP-FC7EKBN\parserdeveloper1 <parserdeveloper@gmail.com>
2021-03-04 13:30:22 -0500

    myframe

---
+++ myFramework/src/main/java/myFramework/myFramework/App.java
@@ -0,0 +1,13 @@
+package myFramework.myFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
